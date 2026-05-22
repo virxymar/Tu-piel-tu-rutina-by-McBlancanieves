1c72f07dc4505151a7fe77fda3ab23c25989c767
virxymar <virxymar@gmail.com>
2026-05-22 18:14:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'mcb-rutina-v10';
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
